0f3d1950568864550b5ef4f4b76ff87e4c61cd5a
Lucas Carlos <lucascarlos945@gmail.com>
2023-12-26 22:41:38 -0300

    CheckMate and CountMoves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,6 +16,7 @@ public class ChessMatch {
 	private Color currentPlayer;
 	private Board board;
 	private boolean check;
+	private boolean checkmate;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -37,6 +38,10 @@ public class ChessMatch {
 	
 	public boolean getCheck() {
 		return check;
+	}
+	
+	public boolean getCheckMate() {
+		return checkmate;
 	}
 	
 
@@ -72,12 +77,19 @@ public class ChessMatch {
 		}
 		
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
+		
+		if(testCheckMate(opponent(currentPlayer))) {
+			checkmate = true;
+		}
+		else {
 		nextTurn();
+		}
 		return (ChessPiece) capturedPiece;
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.PlacePiece(p, target);
 		
@@ -89,7 +101,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.decreaseMoveCount();
 		board.PlacePiece(p, source);
 		
 		if(capturedPiece!=null) {
@@ -153,6 +166,33 @@ public class ChessMatch {
 		return false;
 	
 	}
+	
+	private boolean testCheckMate(Color color) {
+		if(!testCheck(color)) {
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == color).collect(Collectors.toList());
+		
+		for (Piece p: list) {
+			boolean [][] mat = p.possibleMoves();
+			for (int i=0;i< board.getRows(); i++ ) {
+				for ( int j=0;j<board.getColumns(); j++) {
+					if (mat[i][j]) {
+						Position source = ((ChessPiece)p).getChessPosition().toPosition();
+						Position target = new Position(i,j);
+						Piece capturedPiece = makeMove(source,target);
+						boolean testCheck = testCheck(color);
+						undoMove(source,target,capturedPiece);
+						if(!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+			}
+		return true;
+	}
+
 
 	private void placeNewPiece(char colum, int row, ChessPiece piece) {
 		board.PlacePiece(piece, new ChessPosition(colum, row).toPosition());
